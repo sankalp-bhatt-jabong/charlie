63c46ca9e926197485499a3274fd44b5a902425e
Sankalp Bhatt <sankalp.bhatt@jabong.com>
2015-07-26 17:20:49 +0530

    Initialize Repo

---
+++ src/main/java/com/jabong/controllers/AppController.java
@@ -0,0 +1,5 @@
+package com.jabong.controllers;
+
+public class AppController {
+	
+}
---
+++ src/main/java/com/jabong/controllers/UserController.java
@@ -0,0 +1,48 @@
+package com.jabong.controllers;
+
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;  
+
+import com.jabong.controllers.AppController;
+import com.jabong.models.dao.UserDAO;
+import com.jabong.models.User;
+
+/**
+ * Handles requests for the application home page.
+ */
+@Controller
+public class UserController extends AppController{
+	
+	@Autowired
+	private UserDAO userDao;
+	
+	@RequestMapping(value="/")
+	public @ResponseBody
+	List<User> home() {
+	//String home() {
+		
+		List<User> listUsers = userDao.list();
+		
+		return listUsers;
+		//ModelAndView model = new ModelAndView("home");
+		//model.addObject("userList", listUsers);
+		//return model;
+	}
+	
+	@RequestMapping(value="/name")
+	public @ResponseBody
+	List<User> name() {
+	//String home() {
+		
+		List<User> listUsers = userDao.list();
+		
+		return listUsers;
+		//ModelAndView model = new ModelAndView("home");
+		//model.addObject("userList", listUsers);
+		//return model;
+	}
+	
+}
