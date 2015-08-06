44f8f56681980b443ea7e4b83063b6f1e8312bef
Prita Yadav <prita@JADEHOL1286>
2015-08-06 15:07:05 +0530

    BaseResponse checkout from master

--- src/main/java/com/jabong/services/response/BaseResponse.java
+++ src/main/java/com/jabong/services/response/BaseResponse.java
@@ -1,28 +1,5 @@
 package com.jabong.services.response;
 
-<<<<<<< HEAD
-import java.util.*;
-
-
-public class BaseResponse {
-
-   private Object data;
-   private Boolean status;
-   
-   public Boolean getStatus() {
-	return status;
-}
-public void setStatus(Boolean status) {
-	this.status = status;
-}
-public Object getData() {
-	return data;
-}
-public void setData(Object data) {
-	this.data = data;
-}
-}
-=======
 public class BaseResponse {
 
 	private Boolean status;
@@ -45,4 +22,3 @@ public class BaseResponse {
 	}
 
 }
->>>>>>> master
