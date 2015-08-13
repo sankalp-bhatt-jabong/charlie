fa6acb095c9a7307068fd94ccc46111f46cbaa79
Prita Yadav <prita@JADEHOL1286>
2015-08-13 12:11:22 +0530

    changes in StringUtil

--- src/main/java/com/jabong/services/util/StringUtil.java
+++ src/main/java/com/jabong/services/util/StringUtil.java
@@ -1,27 +1,31 @@
 package com.jabong.services.util;
 
-public class StringUtil {
-	/**
-	 * test the value of attribute
-	 * 
-	 * @param result
-	 * @param attribute
-	 * @return boolean
-	 */
-	public static boolean empty(Object value) {
-		boolean test = false;
-		if (value.getClass().isArray()) {
-			Object[] val = (Object[]) value;
-			if (val.length == 0 || val == null) {
-				test = true;
-			}
-		} else {
-			if (value.equals(false) || value.equals("") || value.equals("0")
-					|| value == null) {
-				test = true;
-			}
-		}
-		return test;
-	}
+public class StringUtil
+{
+    /**
+     * test the value of attribute
+     * 
+     * @param result
+     * @param attribute
+     * @return boolean
+     */
+    public static boolean empty(Object value)
+    {
+        if (value == null) {
+            return true;
+        }
+
+        if (value.getClass().isArray()) {
+            Object[] val = (Object[]) value;
+            if (val.length == 0) {
+                return true;
+            }
+        }
+
+        if (value.equals(false) || value.equals("") || value.equals("0")) {
+            return true;
+        }
+        return false;
+    }
 
 }
